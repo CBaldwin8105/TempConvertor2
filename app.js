22e5c8e48976ad1e6856d08538f2eadf97c78e5a
Baldwin <cbaldwin@FVFZMYCQJ1WK.tld>
2022-01-25 14:07:46 -0600

    week4 work

--- app.js
+++ app.js
@@ -2,23 +2,22 @@ var name = prompt("What's your name?")
 
 alert(`Hello ${name}`)
 
-var temp = prompt('Do you want to convert from fahrenheit to celsius (type "F") or celsius to fahrenheit (type "C")?')
+do {
+    var temp = prompt('Do you want to convert from fahrenheit to celsius (type "F") or celsius to fahrenheit (type "C")?')
+} while ((temp != "F") && (temp != "f") && (temp != "fahrenheit") && (temp != "Fahrenheit") && (temp != "C") && (temp != "c") && (temp != "celsius") && (temp != "Celsius"))
+
 if ((temp == "F") || (temp == "f") || (temp == "fahrenheit") || (temp == "Fahrenheit")) {
-    var fahrenheit = Number(prompt("How many degrees do you want to convert from fahrenheit into celsius?"))
-    if (isNaN(fahrenheit)) {
-        alert(`You can only enter a number. Ending program.`)
-    } else {
-        var celsius = ((fahrenheit - 32) * 5 / 9)
-        alert(`${fahrenheit}F° is ${celsius}C°`)
-    }
+    do {
+        var fahrenheit = Number(prompt("How many degrees do you want to convert from fahrenheit into celsius?"))
+    } while (isNaN(fahrenheit))
+    var celsius = ((fahrenheit - 32) * 5 / 9)
+    alert(`${fahrenheit}F° is ${celsius}C°`)
 } else if ((temp == "C") || (temp == "c") || (temp == "celsius") || (temp == "Celsius")) {
-    var celsius = Number(prompt("How many degrees do you want to convert from celsius into fahrenheit?"))
-    if (isNaN(celsius)) {
-        alert(`You can only enter a number. Ending program.`)
-    } else {
-        var fahrenheit = (celsius * 9 / 5 + 32)
-        alert(`${celsius}C° is ${fahrenheit}F°`)
-    }
+    do {
+        var celsius = Number(prompt("How many degrees do you want to convert from celsius into fahrenheit?"))
+    } while (isNaN(celsius))
+    var fahrenheit = (celsius * 9 / 5 + 32)
+    alert(`${celsius}C° is ${fahrenheit}F°`)
 } else {
     alert(`That is not one of the allowable characters. Ending Program.`)
 }
